95d5cf91c07b020c5db207a3ca15901bfc9f8e90
D0rkKnight <shouhanzen@gmail.com>
2022-04-26 01:36:57 -0700

    Accent generation

--- Assets/MusicPlayer.cs
+++ Assets/MusicPlayer.cs
@@ -6,13 +6,13 @@ using UnityEngine.UI;
 public class MusicPlayer : MonoBehaviour
 {
 
-    public class NoteData
+    public class NoteObj
     {
         public GameObject gameObj;
         public float hitTime; // In seconds, as Unity standard
         public Column col;
 
-        public NoteData(GameObject gameObj_, float hitTime_, Column col_)
+        public NoteObj(GameObject gameObj_, float hitTime_, Column col_)
         {
             this.gameObj = gameObj_;
             this.hitTime = hitTime_;
@@ -38,26 +38,32 @@ public class MusicPlayer : MonoBehaviour
     public float bpm = 60;
     private float beatInterval;
     private float lastBeat;
+    private float songStart;
+    private float songStartDelay = 3f;
 
     public float travelSpeed = 5; // In Unity units per second
     public Vector2 dir = new Vector2(0, -1);
     public float hitWindow = 0.5f;
 
+    public float noteAdvance = 5f;
     public float noteTimeout = 3f;
 
     public Text scoreText;
     public int score = 0;
 
-    private List<NoteData> notes;
+    private List<NoteObj> notes;
+    private List<NoteSerializer.Map.Note> noteQueue;
 
     // Start is called before the first frame update
     void Start()
     {
-        notes = new List<NoteData>();
+        notes = new List<NoteObj>();
+        noteQueue = new List<NoteSerializer.Map.Note>();
+
         scoreText.text = "0";
         beatInterval = (float) 60.0 / bpm;
         lastBeat = Time.time;
-
+        songStart = Time.time + songStartDelay;
     }
 
     // Update is called once per frame
@@ -70,13 +76,32 @@ public class MusicPlayer : MonoBehaviour
             onBeat();
         }
 
-        // Only run on channel 1
+        // Load in ready notes
+        List<NoteSerializer.Map.Note> dump = new List<NoteSerializer.Map.Note>();
+        foreach (NoteSerializer.Map.Note n in noteQueue)
+        {
+            float bTime = songStart + (beatInterval * n.beat);
+            
+            if (Time.time + noteAdvance > bTime)
+            {
+                // Load in note
+                GameObject nObj = Instantiate(notePrefab);
+                Column col = columns[n.lane];
+
+                notes.Add(new NoteObj(nObj, bTime, col));
+
+                dump.Add(n);
+            }
+        }
+
+        foreach (NoteSerializer.Map.Note n in dump) noteQueue.Remove(n);
+        dump.Clear();
 
 
         // Kill passed notes
-        List<NoteData> passed = new List<NoteData>();
+        List<NoteObj> passed = new List<NoteObj>();
 
-        foreach (NoteData note in notes)
+        foreach (NoteObj note in notes)
         {
             GameObject col = note.col.gObj;
             Vector2 tPos = col.transform.Find("TriggerBox").position;
@@ -91,7 +116,7 @@ public class MusicPlayer : MonoBehaviour
         }
 
         // Clean dead note buffer
-        foreach (NoteData note in passed) kill(note);
+        foreach (NoteObj note in passed) kill(note);
 
         // Input
         foreach (Column col in columns)
@@ -99,9 +124,9 @@ public class MusicPlayer : MonoBehaviour
             if (Input.GetKeyDown(col.key))
             {
                 // Get bets note within the acceptable input range
-                NoteData bestNote = null;
+                NoteObj bestNote = null;
 
-                foreach (NoteData note in notes)
+                foreach (NoteObj note in notes)
                 {
                     if (!col.Equals(note.col)) continue;
 
@@ -131,7 +156,7 @@ public class MusicPlayer : MonoBehaviour
         }
     }
 
-    private void kill(NoteData note)
+    private void kill(NoteObj note)
     {
         notes.Remove(note);
         Destroy(note.gameObj);
@@ -147,7 +172,7 @@ public class MusicPlayer : MonoBehaviour
     {
 
         // Spawn some sample notes for testing
-        notes.Add(new NoteData(Instantiate(notePrefab), Time.time + 5f, columns[0]));
+        // notes.Add(new NoteObj(Instantiate(notePrefab), Time.time + 5f, columns[0]));
     }
 
     private void highlightCol(Column col, Color c)
@@ -157,5 +182,8 @@ public class MusicPlayer : MonoBehaviour
         rend.color = c;
     }
 
-    //setNotePos(GameObject column, )
+    public void enqueueNote(NoteSerializer.Map.Note note)
+    {
+        noteQueue.Add(note);
+    }
 }
--- Assets/NoteSerializer.cs
+++ Assets/NoteSerializer.cs
@@ -11,13 +11,59 @@ public class NoteSerializer : MonoBehaviour
         HEADER, STREAM, ERR
     }
 
+    public class Map
+    {
+        public struct Note
+        {
+            public int lane;
+            public float beat;
+
+            public Note(int lane_, float beat_)
+            {
+                this.lane = lane_;
+                this.beat = beat_;
+            }
+        }
+
+        public string name;
+        public List<Note> notes;
+
+        // Map is populated after creation
+        public Map()
+        {
+            notes = new List<Note>();
+        }
+
+    }
+
 
     public string fname;
+    private Map map;
+
+    // Acceptable char pool for category data
+    // L-left single R-right single
+    List<char> catPool;
+    List<char> lanePool;
+    char accentChar = '~';
+
+    float beat = 0;
+    MusicPlayer mPlay;
+
+    int lOff = 0;
+    int rOff = 2;
+    int width = 4;
 
     // Start is called before the first frame update
     void Start()
     {
-        MusicPlayer mPlay = GetComponent<MusicPlayer>();
+        // Generate charpools
+        catPool = new List<char>();
+        lanePool = new List<char>();
+        for (char c = 'a'; c <= 'z'; c++) catPool.Add(c);
+        for (char c = 'A'; c <= 'Z'; c++) catPool.Add(c);
+        for (char c = '0'; c <= '9'; c++) lanePool.Add(c);
+
+        mPlay = GetComponent<MusicPlayer>();
 
         string fpath = Application.dataPath + "/Maps/" + fname;
         StreamReader reader = new StreamReader(fpath);
@@ -25,6 +71,8 @@ public class NoteSerializer : MonoBehaviour
 
         string[] tokens = data.Split('\n');
         ParseState state = ParseState.HEADER;
+
+        map = new Map();
 
         foreach (string tok in tokens)
         {
@@ -48,24 +96,125 @@ public class NoteSerializer : MonoBehaviour
 
             }
         }
-        
+
+        // Map is populated now, load into music player
+        foreach (Map.Note n in map.notes) {
+            mPlay.enqueueNote(n);
+        }
     }
 
     private ParseState parseHEADER(string tok)
     {
         if (tok.Equals("streamstart")) return ParseState.STREAM;
+        if (tok.Length == 0) return ParseState.HEADER; 
 
         // Break up a selection of header tags
-        
+        string[] tokSplit = tok.Split(':');
+        if (tokSplit.Length > 1)
+        {
+            // Grab the name
+            string catName = tokSplit[0].Trim();
+
+            // Process header name
+            switch (catName)
+            {
+                case "mapname":
+                    map.name = tokSplit[1].Trim();
+                    break;
+            }
+
+            return ParseState.HEADER;
+        } else
+        {
+            Debug.LogError("Poor delimiter for token with " + tokSplit.Length + " sections.");
+        }
+
         return ParseState.ERR;
     }
 
+
     private ParseState parseSTREAM(string tok)
     {
-        switch (tok)
+        // Single note reader
+        // Read a column
+        int i = 0;
+        string col = "";
+        while (i<tok.Length)
         {
-            default:
-                return ParseState.ERR;
+            if (catPool.Contains(tok[i])) col += tok[i];
+            else break;
+
+            i++;
         }
+
+        switch (col)
+        {
+            case "R":
+            case "L":
+                break;
+            default:
+                Debug.LogError("Illegal category: " + col);
+                break;
+        }
+
+        // Read lane
+        string lane = "";
+        while (i<tok.Length)
+        {
+            if (lanePool.Contains(tok[i])) lane += tok[i];
+            else break;
+
+            i++;
+        }
+
+        // Read accent (can have different amounts)
+        int accent = 0;
+        while (i < tok.Length)
+        {
+            if (tok[i] == accentChar) accent++;
+            else break;
+
+            i++;
+        }
+
+        // Empty = no lane specifier, defaults to the left lane of the category
+        int l = 0;
+        if (lane.Length > 0) l = int.Parse(lane)-1;
+
+        // Given lane weights, calculate target lane
+        switch (col)
+        {
+            case "R":
+                l += rOff;
+                break;
+            case "L":
+                l += lOff;
+                break;
+        }
+
+        // Double/triple up according to accents
+        if (accent > 0) {
+
+            // Count number of valid colums
+            int validCols = 0;
+            int leftValid = Mathf.Max(0, l - accent);
+            for (int j = leftValid; j <= l; j++)
+            {
+                if (j + accent >= width) break;
+                validCols++;
+            }
+
+            int sCol = Random.Range(leftValid, leftValid + validCols);
+
+            for (int j=0; j<=accent; j++) map.notes.Add(new Map.Note(sCol + j, beat));
+
+        } else {
+            map.notes.Add(new Map.Note(l, beat));
+        }
+
+        // Normally look for a beat specifier, just advance beat here
+        beat++;
+
+        return ParseState.STREAM; // Persist state
     }
 }
